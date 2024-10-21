9b3cb17e9921c8be31e470f2911a3f25cbc6b8f3
Jose Sanchez <jose990.jsn@gmail.com>
2024-10-21 23:45:58 +0200

    fix: restore plugins

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,9 @@
 module.exports = {
     'strapi-neon-tech-db-branches': {
-        enabled: true
+        enabled: true,
+        neonApiKey: "t9t3e6j2k4c6g0bxhrf969wah1uad08vh3cdeqdcwz8icndm94roxjk0boj0hxc8", // get it from here: https://console.neon.tech/app/settings/api-keys
+        neonProjectName: "ecommerce-vinyl", // the neon project under wich your DB runs
+        neonRole: "neondb_owner", // create it manually under roles for your project first
+        gitBranch: "main",
     },
 }
